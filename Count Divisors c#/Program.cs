de57d5c8e54080ea8cc85f0865df0f71de57420c
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-09-05 17:32:20 -0400

    Add project files.

---
+++ Count Divisors c#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // قراءة المدخلات
+        string[] input = Console.ReadLine().Split(' ');
+        int l = int.Parse(input[0]);
+        int r = int.Parse(input[1]);
+        int k = int.Parse(input[2]);
+
+        // حساب عدد الأرقام التي تقبل القسمة على k
+        int count = 0;
+        for (int i = l; i <= r; i++)
+        {
+            if (i % k == 0)
+            {
+                count++;
+            }
+        }
+
+        // طباعة النتيجة
+        Console.WriteLine(count);
+    }
+}
+
